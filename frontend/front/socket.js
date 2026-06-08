cbf228c051530c99ba8e359fafc7081d57bc4277
Murari Bhavya Sree <muraribhavyasree19@gmail.com>
2026-06-08 22:09:34 +0530

    project1

--- frontend/front/socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io("https://crime-snap-main-764y.onrender.com", {
-  transports: ["websocket", "polling"],
-});
-
-export default socket;
--- frontend/front/src/socket.js
+++ frontend/front/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://crime-snap-main-764y.onrender.com", {
-  transports: ["websocket"],
+  transports: ["websocket", "polling"],
 });
 
 export default socket;
