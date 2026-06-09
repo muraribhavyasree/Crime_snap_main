9cf4299e90ab50f4922b4b467e5cb2dd7eae5dda
Murari Bhavya Sree <muraribhavyasree19@gmail.com>
2026-06-09 11:54:08 +0530

    6

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 /* ================= MIDDLEWARE ================= */
 app.use(cors({
   origin: [
-    "https://crimesnap7.netlify.app",
+    "https://ccrimesnap7.netlify.app",
     "https://crimesnap26.netlify.app",
   ],
   credentials: true
